5a3e50ee3ee2b34ded86fb851d0c48b2557b9092
ab317bas <145981083+ab317bas@users.noreply.github.com>
2023-09-25 15:20:15 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,35 +1,28 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public static void main(String[] args) {
+        // Press Alt+Enter with your caret at the highlighted text to see how
+        // IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
 
-    public static void selectionSort(int[] arr){
-        for (int i = 0; i < arr.length - 1; i++)
-        {
-            int index = i;
-            for (int j = i + 1; j < arr.length; j++){
-                if (arr[j] < arr[index]){
-                    index = j;//searching for lowest index
-                }
+        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+        }
+    }
+    public static void insertionSort(int array[]) {
+        int n = array.length;
+        for (int j = 1; j < n; j++) {
+            int key = array[j];
+            int i = j-1;
+            while ( (i > -1) && ( array [i] > key ) ) {
+                array [i+1] = array [i];
+                i--;
             }
-            int smallerNumber = arr[index];
-            arr[index] = arr[i];
-            arr[i] = smallerNumber;
+            array[i+1] = key;
         }
-    }
-
-    public static void main(String a[]){
-        int[] arr1 = {9,14,3,2,43,11,58,22};
-        System.out.println("Before Selection Sort");
-        for(int i:arr1){
-            System.out.print(i+" ");
-        }
-        System.out.println();
-
-        selectionSort(arr1);//sorting array using selection sort
-
-        System.out.println("After Selection Sort");
-        for(int i:arr1){
-            System.out.print(i+" ");
-        }
-    }
-}
+    } }
